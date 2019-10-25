c0a9e3eca5bb90affafd1f670a4faf663fe55e3e
Pejiite <tristan.guillemot1@gmail.com>
2019-10-25 12:38:21 +0200

    Ajout sous titre css

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-l
+function init(){var n=document.querySelector(".expander").querySelector(".btn"),t=document.querySelector(".expander__content");n.addEventListener("click",function(){t.style.display="block"})}init();
