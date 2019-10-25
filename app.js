a6094ac27f2f8bbc50f80023f127cecd000ea2ed
enzo arnaudet <aenzo09@hotmail.fr>
2019-10-25 15:48:40 +0200

    ethan

--- app.js
+++ app.js
@@ -1,29 +1,8 @@
-<<<<<<< HEAD
-function init(){var n=document.querySelector(".expander").querySelector(".btn"),t=document.querySelector(".expander__content");n.addEventListener("click",function(){t.style.display="block"})}init();
-=======
-
-
-var bigtitle = document.queryselector('#bigtitle');
-var secondtitles= document.queryselectorall('h2');
-
-
-
-
-
-function onClickBigTitle(){
-	console.log('hello')
-}
-
-bigtitle.addEventlistener('click', onClickBigTitle);
-
-
-
-
-
-
-function onClickBigTitle() {
-	bigtitle.style.color = "blue";
-}
-
-
->>>>>>> af30aa7eef4be1d397b8c8ad9677b0be680a6da0
+$(document).ready(function(){
+  $('.slider1').bxSlider({
+    slideWidth: 215,
+    minSlides: 2,
+    maxSlides: 5,
+    slideMargin: 10
+  });
+});
