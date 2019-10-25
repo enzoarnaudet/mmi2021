c949c56acb19948d85918a2c340b0c158a9061a5
Pejiite <tristan.guillemot1@gmail.com>
2019-10-25 15:07:14 +0200

    okkk

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 <<<<<<< HEAD
-function init(){var n=document.querySelector(".expander").querySelector(".btn"),t=document.querySelector(".expander__content");n.addEventListener("click",function(){t.style.display="block"})}init();
 =======
 
 
